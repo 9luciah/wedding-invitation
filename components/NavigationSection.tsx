7c26cd373eda45d90a81412370f68af073a18d1b
seulye <9luciah@gmail.com>
2024-01-10 21:52:07 +0900

    space %20으로 인코딩

--- components/NavigationSection.tsx
+++ components/NavigationSection.tsx
@@ -4,13 +4,13 @@ export default function NavigationSection() {
   return (
     <ul>
       <li>
-        <a href="http://app.map.naver.com/launchApp/?version=11&menu=navigation&elat=37.5056056&elng=126.8838985&etitle=%eb%8d%94%eb%a7%81%ed%81%ac%ed%98%b8%ed%85%94+%ec%84%9c%ec%9a%b8">
+        <a href="http://app.map.naver.com/launchApp/?version=11&menu=navigation&elat=37.5056056&elng=126.8838985&etitle=%eb%8d%94%eb%a7%81%ed%81%ac%ed%98%b8%ed%85%94%20%ec%84%9c%ec%9a%b8">
           <div>
             <img alt="/images/ico_navermap.png" src="/images/ico_navermap.png" />
             <span>네이버지도</span>
           </div>
         </a>
-        <a href="https://apis.openapi.sk.com/tmap/app/routes?appKey=MzpTeqQznV5KMVMOxMCPu7pxZP2sQKfF1557407z&name=%eb%8d%94%eb%a7%81%ed%81%ac%ed%98%b8%ed%85%94+%ec%84%9c%ec%9a%b8&lon=126.8838985&lat=37.5056056">
+        <a href="https://apis.openapi.sk.com/tmap/app/routes?appKey=MzpTeqQznV5KMVMOxMCPu7pxZP2sQKfF1557407z&name=%eb%8d%94%eb%a7%81%ed%81%ac%ed%98%b8%ed%85%94%20%ec%84%9c%ec%9a%b8&lon=126.8838985&lat=37.5056056">
           <div>
             <img alt="/images/ico_tmap.png" src="/images/ico_tmap.png" />
             <span>티맵</span>
