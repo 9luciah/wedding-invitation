8a02b1087fcc794934237f08697b2a025ed913c8
seulye <9luciah@gmail.com>
2024-01-16 17:48:50 +0900

    캘린더 완성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable camelcase */
 import React from 'react';
 import { styled } from 'styled-components';
@@ -8,16 +9,15 @@ import GallerySection from '../components/GallerySection';
 import MapSection from '../components/MapSection';
 import NavigationSection from '../components/NavigationSection';
 import ContactSection from '../components/ContactSection';
-import Calendar from '../components/Calendar';
 import KakaoTalkShare from '../components/KakaoTalkShare';
 
 export default function Home() {
   return (
     <Container>
       <script
-        src="https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js"
-        integrity="sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8"
-        crossOrigin="anonymous"
+        src={'https://t1.kakaocdn.net/kakao_js_sdk/2.6.0/kakao.min.js'}
+        integrity={'sha384-6MFdIr0zOira1CHQkedUqJVql0YtcZA1P0nbPrQYJXVJZUkTk/oX4U9GhUIs3/z8'}
+        crossOrigin={'anonymous'}
       />
       <Wrapper>
         <GlobalStyle />
@@ -28,7 +28,82 @@ export default function Home() {
         <Image src={'/images/wiz3.jpeg'} />
         <NameSection />
         <TextContentSection />
-        <GallerySection />
+        <div>
+          <GallerySection />
+        </div>
+        <CalendarContainer>
+          <TitleSection>
+            <TitleDate>{'2024.03.09'}</TitleDate>
+            <TitleDay>{'토요일 오후 2시'}</TitleDay>
+          </TitleSection>
+          <CalendarWrapper>
+            <CalendarTable>
+              <thead>
+                <tr>
+                  <th className={'pinkText'}>{'일'}</th>
+                  <th>{'월'}</th>
+                  <th>{'화'}</th>
+                  <th>{'수'}</th>
+                  <th>{'목'}</th>
+                  <th>{'금'}</th>
+                  <th>{'토'}</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                  <td />
+                  <td className={'pinkText'}>{'1'}</td>
+                  <td>{'2'}</td>
+                </tr>
+                <tr>
+                  <td className={'pinkText'}>{'3'}</td>
+                  <td>{'4'}</td>
+                  <td>{'5'}</td>
+                  <td>{'6'}</td>
+                  <td>{'7'}</td>
+                  <td>{'8'}</td>
+                  <td className={'selected'}>
+                    <span>{'9'}</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td className={'pinkText'}>{'10'}</td>
+                  <td>{'11'}</td>
+                  <td>{'12'}</td>
+                  <td>{'13'}</td>
+                  <td>{'14'}</td>
+                  <td>{'15'}</td>
+                  <td>{'16'}</td>
+                </tr>
+                <tr>
+                  <td className={'pinkText'}>{'17'}</td>
+                  <td>{'18'}</td>
+                  <td>{'19'}</td>
+                  <td>{'20'}</td>
+                  <td>{'21'}</td>
+                  <td>{'22'}</td>
+                  <td>{'23'}</td>
+                </tr>
+                <tr>
+                  <td className={'pinkText'}>{'24'}</td>
+                  <td>{'25'}</td>
+                  <td>{'26'}</td>
+                  <td>{'27'}</td>
+                  <td>{'28'}</td>
+                  <td>{'29'}</td>
+                  <td>{'30'}</td>
+                </tr>
+                <tr>
+                  <td className={'pinkText'}>{'31'}</td>
+                </tr>
+              </tbody>
+            </CalendarTable>
+          </CalendarWrapper>
+        </CalendarContainer>
         <div>
           <MapSection />
         </div>
@@ -38,9 +113,6 @@ export default function Home() {
         <ContactContainer>
           <ContactSection />
         </ContactContainer>
-        <div>
-          <Calendar />
-        </div>
         <KakaoTalkShare />
       </Wrapper>
     </Container>
@@ -196,5 +268,74 @@ const ContactContainer = styled.div`
 
   ul > li > span > a > img {
     width: 32px;
+  }
+`;
+
+const CalendarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  width: 100%;
+  margin-top: 80px;
+`;
+
+const TitleSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const TitleDate = styled.span`
+  font-size: 21.5px;
+  opacity: 0.95;
+  color: #544f4f;
+  letter-spacing: 1px;
+  font-size: 19.6px;
+`;
+
+const TitleDay = styled.span`
+  margin-top: 7px;
+  font-size: 16px;
+  color: #544f4f;
+  letter-spacing: 0;
+  opacity: 0.8;
+  font-size: 13px;
+`;
+
+const CalendarWrapper = styled.div`
+  display: flex;
+  width: 90%;
+  height: 280px;
+  padding: 15px 0;
+  margin: 35px 0;
+  border-top: 1px solid #000;
+  border-bottom: 1px solid #000;
+  border-color: #eee;
+`;
+
+const CalendarTable = styled.table`
+  width: 100%;
+  margin: 0 auto;
+  margin-bottom: 20px;
+
+  border-collapse: collapse;
+  border-spacing: 0;
+
+  th {
+    font-weight: normal;
+    height: 50px;
+  }
+
+  .pinkText {
+    color: #f79e9e;
+  }
+
+  .selected {
+    width: 35px;
+    height: 35px;
+    color: white;
+    background-color: #f79e9e;
+    border-radius: 50%;
   }
 `;
